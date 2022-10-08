01c883cf7a2a6ceaa9f8cbf8fb1ddb8eb40dc1f0
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-10-08 23:54:07 +0300

    add homework

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,4 @@
-export const fetchCountries=name=>{
-   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response=>response.json())
+export const fetchCountries = name => {
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
 }
+
--- src/index.js
+++ src/index.js
@@ -9,37 +9,47 @@ const input = document.querySelector('#search-box');
 const debounce = require('lodash.debounce');
 const countryList = document.querySelector('.country-list');
 const coutnryInfo = document.querySelector('.country-info');
-const lengthAudit = countries => {
+function lengthAudit(countries){
+    clearList()
     if (countries.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        clearList();
+        ifManyCountries()
     }
-    else if (countries.length > 2 && countries.length < 10) {
-        countryList.insertAdjacentHTML('beforeend', countries.map(manyCountries).join(''));
-        coutnryInfo.innerHTML='';
-       
+    else if (countries.length >= 2 && countries.length <= 10) {
+        countriesList(countries.map(manyCountries))
     }
     else if (countries.length === 1) {
-        coutnryInfo.insertAdjacentHTML('beforeend', countries.map(oneCountry) );
-        countryList.innerHTML='';
-        
-    }
-    else if (!countries.length) {
-        error();
+        shortList(countries.map(oneCountry));
     }
 }
-
-const onInputIn = event => {
-    fetchCountries(event.target.value.trim()).then(lengthAudit);
+function onInputIn (event){
+    fetchCountries(event.target.value.trim()).then(response => {
+    if (!response.ok&&event.target.value.trim()!=='') {
+      throw new Error(response.status);
+    }
+    return response.json();
+  }).then(lengthAudit).catch(error);
 }
-input.addEventListener('input', debounce(onInputIn, DEBOUNCE_DELAY));
+function error(err) {
+        console.log(err);
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        clearList();
+}
 
-function error() {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
+function ifManyCountries() {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     clearList();
-    
 }
+    
 function clearList() {
     countryList.innerHTML ='';
     coutnryInfo.innerHTML='';
 }
+
+function shortList(country) {
+    coutnryInfo.insertAdjacentHTML('beforeend', country);
+}
+
+function countriesList(list) {
+    countryList.insertAdjacentHTML('beforeend', list.join(''));
+}
+input.addEventListener('input', debounce(onInputIn, DEBOUNCE_DELAY));
